dadc1ec38efd7c2f88fee5d1030a4b5c270e9951
RickDev <henriiisilva2007@gmail.com>
2026-05-31 20:09:58 -0300

    Ajuste de EndPoints no controller

--- src/main/java/com/estudos/Produtos/Controller/ProdutosController.java
+++ src/main/java/com/estudos/Produtos/Controller/ProdutosController.java
@@ -3,11 +3,9 @@ package com.estudos.Produtos.Controller;
 import com.estudos.Produtos.Dto.ProdutosDto;
 import com.estudos.Produtos.Model.ProdutosModel;
 import com.estudos.Produtos.Service.ProdutosService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -25,28 +23,33 @@ public class ProdutosController {
     }
 
 
-    public ResponseEntity<ProdutosModel> salvar(@RequestBody ProdutosDto dto, UriComponentsBuilder uriBuilder){
+    @PostMapping
+    public ResponseEntity<ProdutosModel> salvar(@Valid @RequestBody ProdutosDto dto, UriComponentsBuilder uriBuilder){
         var produtos = produtosService.salvar(dto);
         var uri = uriBuilder.path("/api/{id}").buildAndExpand(produtos.getId()).toUri();
         return ResponseEntity.created(uri).body(produtos);
     }
 
 
+    @GetMapping
     public ResponseEntity<List<ProdutosModel>> listar() {
         var produto = produtosService.Listar();
         return ResponseEntity.status(201).body(produto);
     }
 
+    @GetMapping("/{id}")
     public ResponseEntity<ProdutosModel> acharPorId(@PathVariable Long id){
         var produto = produtosService.acharPorId(id);
         return ResponseEntity.status(201).body(produto);
     }
 
+    @PutMapping("/{id}")
     public ResponseEntity<ProdutosModel> atualizarPorId(@PathVariable Long id, @RequestBody ProdutosDto dto){
         var atualizar = produtosService.atualizarPorId(id, dto);
         return ResponseEntity.status(201).body(atualizar);
     }
 
+    @DeleteMapping("/{id}}")
     public ResponseEntity<Void> deletarPorId(@PathVariable Long id){
         return ResponseEntity.noContent().build();
     }
