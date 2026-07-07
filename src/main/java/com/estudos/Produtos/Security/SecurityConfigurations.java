27c845d64cc6835ab1c4dbe5f32c4396f20795bf
RickDev <henriiisilva2007@gmail.com>
2026-07-06 21:56:30 -0300

    Adicao de SpringConfigurations

---
+++ src/main/java/com/estudos/Produtos/Security/SecurityConfigurations.java
@@ -0,0 +1,27 @@
+package com.estudos.Produtos.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm ->
+                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+}
