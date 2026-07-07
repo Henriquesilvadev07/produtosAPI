49f940859f898ed1b59c53ea5cb131f92df4b7f1
RickDev <henriiisilva2007@gmail.com>
2026-07-06 22:02:43 -0300

    ajuste na anotacao requiredArgs e private final

--- src/main/java/com/estudos/Produtos/Service/UsuariosService.java
+++ src/main/java/com/estudos/Produtos/Service/UsuariosService.java
@@ -3,17 +3,17 @@ package com.estudos.Produtos.Service;
 import com.estudos.Produtos.Repository.UsuariosRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class UsuariosService implements UserDetailsService {
 
-    private UsuariosRepository usuariosRepository;
+    private final UsuariosRepository usuariosRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
