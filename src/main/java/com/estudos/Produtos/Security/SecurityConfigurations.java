41d9714af3164735333517df4b264fa9712033d5
RickDev <henriiisilva2007@gmail.com>
2026-07-09 21:11:59 -0300

    Correçao de anotaçao @Bean

--- src/main/java/com/estudos/Produtos/Security/SecurityConfigurations.java
+++ src/main/java/com/estudos/Produtos/Security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.estudos.Produtos.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfigurations {
 
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
