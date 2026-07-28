1782181051cca9f2e903960ce6536bd58aac2bc7
RickDev <henriiisilva2007@gmail.com>
2026-07-27 22:20:15 -0300

    Adicao de autenticacao no securityFilter

--- src/main/java/com/estudos/Produtos/Security/SecurityFilter.java
+++ src/main/java/com/estudos/Produtos/Security/SecurityFilter.java
@@ -1,11 +1,15 @@
 package com.estudos.Produtos.Security;
 
+import com.estudos.Produtos.Repository.UsuariosRepository;
 import com.estudos.Produtos.Service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,13 +21,17 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
 
+    private final UsuariosRepository usuariosRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
        var tokenJWT = recuperarToken(request);
 
        if (tokenJWT != null) {
            var subject = tokenService.getSubject(tokenJWT);
-           System.out.println(subject);
+           var usuario = usuariosRepository.findByLogin(subject);
+           var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+           SecurityContextHolder.getContext().setAuthentication(authentication);
        }
 
         filterChain.doFilter(request,response);
