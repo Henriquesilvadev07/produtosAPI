4514c2f8cc33fbf51836861d82a29f655529a3da
RickDev <henriiisilva2007@gmail.com>
2026-07-27 22:58:36 -0300

    Adicao de ordem de filtragem no SecurityConfigurations e finalizacao de modulo de seguranca

--- src/main/java/com/estudos/Produtos/Security/SecurityConfigurations.java
+++ src/main/java/com/estudos/Produtos/Security/SecurityConfigurations.java
@@ -1,5 +1,5 @@
 package com.estudos.Produtos.Security;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,12 +11,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.beans.Encoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfigurations {
+
+    private final SecurityFilter securityFilter;
 
 
     @Bean
@@ -30,6 +32,7 @@ public class SecurityConfigurations {
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
