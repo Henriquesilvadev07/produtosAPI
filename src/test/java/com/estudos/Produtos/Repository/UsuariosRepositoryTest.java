5c20108f2de20c0c847b6c39c7f9a7a7ecd43a1f
RickDev <henriiisilva2007@gmail.com>
2026-08-30 14:09:50 -0300

    Adicao do banco de dados H2 em memoria para testes

---
+++ src/test/java/com/estudos/Produtos/Repository/UsuariosRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.estudos.Produtos.Repository;
+
+import lombok.Data;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class UsuariosRepositoryTest {
+
+
+    void
+
+}
