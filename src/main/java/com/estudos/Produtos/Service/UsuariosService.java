4233316b71d6e63d39166ceb68e1f39697c7cafb
RickDev <henriiisilva2007@gmail.com>
2026-08-30 14:01:08 -0300

    Ajuste de exception utilizando UsernameNotFound para reduçao de codigo e boa pratica

--- src/main/java/com/estudos/Produtos/Service/UsuariosService.java
+++ src/main/java/com/estudos/Produtos/Service/UsuariosService.java
@@ -17,11 +17,9 @@ public class UsuariosService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDetails user = usuariosRepository.findByLogin(username);
+        UserDetails user = usuariosRepository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario nao encontrado"));
 
-        if (user == null) {
-            throw new UsernameNotFoundException("Usuario nao encontrado");
-        }
         return user;
     }
 }
