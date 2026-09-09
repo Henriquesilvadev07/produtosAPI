50d2adfd6091bda5df8267ccbcd72d466c948b2f
RickDev <henriiisilva2007@gmail.com>
2026-09-08 21:51:25 -0300

    Criacao da classe de teste do service de produtos

---
+++ src/test/java/com/estudos/Produtos/Service/ProdutosServiceTest.java
@@ -0,0 +1,18 @@
+package com.estudos.Produtos.Service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProdutosServiceTest {
+
+    @Test
+    void salvar() {
+
+    }
+
+    @Test
+    void listar() {
+    }
+}
