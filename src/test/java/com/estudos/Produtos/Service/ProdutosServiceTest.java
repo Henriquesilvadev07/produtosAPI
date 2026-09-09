838a431959351c1697abf240bd6d3cc95dc55972
RickDev <henriiisilva2007@gmail.com>
2026-09-08 22:47:31 -0300

    finalizacao do teste unitario de salvar os produtos

--- src/test/java/com/estudos/Produtos/Service/ProdutosServiceTest.java
+++ src/test/java/com/estudos/Produtos/Service/ProdutosServiceTest.java
@@ -1,18 +1,67 @@
 package com.estudos.Produtos.Service;
 
+import com.estudos.Produtos.Dto.ProdutosDto;
+import com.estudos.Produtos.Model.CategoriaEnum;
+import com.estudos.Produtos.Model.ProdutosModel;
+import com.estudos.Produtos.Repository.ProdutosRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
 class ProdutosServiceTest {
 
-    @Test
-    void salvar() {
+    @Mock
+    private ProdutosRepository produtosRepository;
 
+    @InjectMocks
+    private ProdutosService produtosService;
+
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    @DisplayName("Should save produtos")
+    void salvar() {
+        ProdutosDto dto = new ProdutosDto(
+                "sabao",
+                "sabonete de banho",
+                BigDecimal.valueOf(2.99),
+                CategoriaEnum.OUTROS,
+                300);
+        ProdutosModel produtos = new ProdutosModel();
+        produtos.setNome(dto.nome());
+        produtos.setCategoria(dto.categoria());
+        produtos.setDescricao(dto.descricao());
+        produtos.setPreco(dto.preco());
+        produtos.setEstoque(dto.estoque());
+
+        when(produtosRepository.save(any(ProdutosModel.class))).thenReturn(produtos);
+
+        ProdutosModel produtosSalvos = produtosService.salvar(dto);
+
+        assertNotNull(produtosSalvos);
+        assertEquals("sabao", produtosSalvos.getNome());
     }
 
     @Test
+    @DisplayName("Should list all products saved")
     void listar() {
     }
 }
